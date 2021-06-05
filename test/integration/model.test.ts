2a3e752ab5600ec2d409920affd7b9153f8d85d6
Siziy Ivan <cziy@ya.ru>
2021-06-05 17:42:30 +0300

    test

--- test/integration/model.test.ts
+++ test/integration/model.test.ts
@@ -4,9 +4,9 @@ let orderInstance: Model;
 describe("Model", function () {
   this.timeout(10000);
 
-  describe("model order", async function () {
-    it("Exist next() method", async function () {
-      orderInstance = await Order.create({}).fetch();
+  describe("model order", function () {
+    it("Exist next() method",  function () {
+      orderInstance = Order.create({}).fetch();
       console.log(orderInstance);
       //sails.models["order"].next();
     });
