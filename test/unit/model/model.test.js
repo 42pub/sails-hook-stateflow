0d8f907a03cb26dc21bc01b063dad60380224e6d
Siziy Ivan <cziy@ya.ru>
2021-06-05 16:36:29 +0300

    add test fixtures

---
+++ test/unit/model/model.test.js
@@ -0,0 +1,8 @@
+describe('Model', function () {
+    this.timeout(10000);
+    describe('New Example', function () {
+        it('new it', function () {
+            return true;
+        });
+    });
+});
---
+++ test/unit/model/model.test.ts
@@ -0,0 +1,10 @@
+describe('Model',function () {
+  this.timeout(10000);
+  
+  describe('New Example', function (){
+    it('new it', function(){
+      return true;
+    });
+  });
+
+});
