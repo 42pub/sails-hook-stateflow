71c46e10b33bf1cee468d03862c2bb25b888b2a1
Siziy Ivan <cziy@ya.ru>
2021-06-27 14:52:26 +0300

    fix next

--- waterline/model.js
+++ waterline/model.js
@@ -32,7 +32,8 @@ module.exports = function (config) {
             catch (error) {
                 throw `move to ${nextState} ended with error: ${error}`;
             }
-            await sails.models[modelname].state[modelInstanceData[nextState]].runInState(modelInstanceData);
+            console.log(nextState, sails.models[modelname].state[nextState]);
+            await sails.models[modelname].state[nextState].runInState(modelInstanceData);
             let update = {};
             update[stateField] = nextState;
             modelInstanceData = (await this.update(criteria, update).fetch())[0];
--- waterline/model.ts
+++ waterline/model.ts
@@ -36,8 +36,8 @@ module.exports = function(config) {
       } catch (error) {
         throw `move to ${nextState} ended with error: ${error}`
       }  
-
-      await sails.models[modelname].state[modelInstanceData[nextState]].runInState(modelInstanceData)
+      console.log(nextState,sails.models[modelname].state[nextState] )
+      await sails.models[modelname].state[nextState].runInState(modelInstanceData)
       
       let update = {}
       update[stateField] = nextState;
